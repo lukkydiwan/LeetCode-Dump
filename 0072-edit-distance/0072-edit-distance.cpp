b5649203b011acfd0a0b3579fd6f06619f575810
lukkydiwan <diwanlukky@gmail.com>
2026-07-25 14:43:46 +0530

    Time: 63 ms (9.33%), Space: 83.2 MB (7.25%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int ans(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(i<0)return j+1;
+        if(j<0)return i+1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j])return dp[i][j]=ans(i-1,j-1,s,t,dp);
+        else{
+            int insert=1+ans(i,j-1,s,t,dp);
+            int del=1+ans(i-1,j,s,t,dp);
+            int replace=1+ans(i-1,j-1,s,t,dp);
+            return dp[i][j]=min({insert,del,replace});
+        }
+    }
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
+        return ans(word1.size()-1, word2.size()-1,word1, word2,dp);
+    }
+};
