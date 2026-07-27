e57a59460393af4154e79a324a725a52de3f98fa
lukkydiwan <diwanlukky@gmail.com>
2026-07-27 23:21:03 +0530

    Time: 3 ms (83.57%), Space: 18.3 MB (28.68%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -10,8 +10,19 @@ public:
             return dp[i][buy][k]=max(p[i]+ans(i+1,1,k-1,p,dp),ans(i+1,0,k,p,dp));
         }
     }
-    int maxProfit(int k, vector<int>& p) {
-        vector<vector<vector<int>>> dp(p.size(), vector<vector<int>> (2,vector<int> (k+1,-1)));
-        return ans(0,1,k,p,dp);
+    int maxProfit(int kt, vector<int>& p) {
+        vector<vector<vector<int>>> dp(p.size()+1, vector<vector<int>> (2,vector<int> (kt+1,0)));
+        for(int i=p.size()-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int k=1; k<=kt; k++){
+                    if(buy){
+                        dp[i][buy][k]=max(-p[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }else{
+                        dp[i][buy][k]=max(p[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][kt];
     }
 };
