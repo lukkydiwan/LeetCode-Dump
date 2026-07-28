3b4223842cbaf211b08a944d6a14206680f03b62
lukkydiwan <diwanlukky@gmail.com>
2026-07-28 08:00:45 +0530

    Time: 109 ms (24.83%), Space: 70 MB (58.9%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        map<int,int> m;
-        for(auto x:s){
-            m[x]++;
-        }
-        string ans="",real;
         if(s.size()%2==0){
-            for(auto x:m){
-                for(int i=0; i<x.second/2; i++){
-                        ans+=x.first;
-                    }
-            }
-            real=ans;
-            reverse(ans.begin(), ans.end());
-            real+=ans;
-            return real;
+            string half = s.substr(0,s.size()/2);
+            sort(half.begin(), half.end());
+            string ans=half;
+            reverse(half.begin(),half.end());
+            return ans+half;
         }else{
-            char c;
-            for(auto x:m){
-                if(x.second>=2){
-                    for(int i=0; i<x.second/2; i++){
-                        ans+=x.first;
-                    }
-                }
-                if(x.second%2==1){
-                    c=x.first;
-                }
-            }
-                real=ans;
-                real+=c;
-                reverse(ans.begin(), ans.end());
-                real+=ans;
-                return real;
+            string half = s.substr(0,s.size()/2);
+            sort(half.begin(), half.end());
+            string ans=half;
+            ans+=s[(s.size()-1)/2];
+            reverse(half.begin(),half.end());
+            return ans+half;
+
         }
         return s;
     }
