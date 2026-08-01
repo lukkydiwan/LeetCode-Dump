ff77ec96fdbf4aab8b0e840ce3073fefdef0337d
lukkydiwan <diwanlukky@gmail.com>
2026-08-01 10:31:26 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int ans(int st, int end, vector<int> &nums){
+    int ans(int st, int end, vector<int> &nums,vector<vector<int>> &dp){
         if(st==end){
             return nums[st];
         }
-       int takeL=nums[st]-ans(st+1,end,nums);
-       int takeR=nums[end]-ans(st,end-1,nums);
-       return max(takeL,takeR);
+        if(dp[st][end]!=INT_MAX)return dp[st][end];
+       int takeL=nums[st]-ans(st+1,end,nums,dp);
+       int takeR=nums[end]-ans(st,end-1,nums,dp);
+       return dp[st][end]=max(takeL,takeR);
     }
     bool predictTheWinner(vector<int>& nums) {
         if(nums.size()<=2)return true;
-        int p1=ans(0,nums.size()-1,nums);
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),INT_MAX));
+        int p1=ans(0,nums.size()-1,nums,dp);
         return p1>=0;
     }
 };
